5033861a565379741e9d3d1a6aedc679d58a88cf
Bagwells <timmyayomidebagwells@outlook.com>
2026-03-15 22:56:41 +0100

    Upt: BTrust Bitcoin-cli week One exercise

--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,3 @@
 # Check the total amount in the wallet.
 
-bitcoin-cli -regtest -rpcwallet=builderswallet getbalance | tr -d ' \n\t\r'
+bitcoin-cli -regtest -rpcwallet=builderswallet getbalance | bc -1
