636789536167087df425ab3ff89bf643c6bf7467
Bagwells <timmyayomidebagwells@outlook.com>
2026-03-16 20:25:51 +0100

    Upt: BTrust Bitcoin-cli week One exercise

--- submission/04.sh
+++ submission/04.sh
@@ -1,5 +1,4 @@
 # Create a wallet with the name "builderswallet".
 
-builders_Addr=$(bitcoin-cli -regtest createwallet "builderswallet")
+bitcoin-cli -regtest createwallet "builderswallet"
 
-bitcoin-cli -regtest generate 101 "$builders_Addr"
--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,6 @@
 # Check the total amount in the wallet.
 
+builders_Addr=$(bitcoin-cli -regtest getnewaddress)
+bitcoin-cli -regtest generate 101 "$builders_Addr"
+
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
